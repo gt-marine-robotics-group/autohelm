a21a7cc00a8d9fb160948c849206b81f61e7da17
Sean Fish <seantfish@gmail.com>
2025-09-06 23:49:59 -0400

    All in proto py

--- autohelm/proto.py
+++ autohelm/proto.py
@@ -10,27 +10,26 @@ PBGEN_MC = PACKAGE_DIR / 'gen' / 'pbmc'
 
 NANOPB_PLUGIN = Path(sys.executable).parent / 'protoc-gen-nanopb'
 
-def build_protobuf(py=True, mc=True):
+def build_protobuf():
     """Build Protobuf"""
     PBGEN_PY.mkdir(parents=True, exist_ok=True)
     PBGEN_MC.mkdir(parents=True, exist_ok=True)
 
     for pb_file in PB_DIR.glob('*.proto'):
-        if py:
-            run([
-                'protoc',
-                f'-I={PB_DIR}',
-                f'--python_out={PBGEN_PY}',
-                str(pb_file)
-            ], check=True)
-        elif mc:
-            run([
-                'protoc',
-                f'-I={PB_DIR}',
-                f'--plugin=protoc-gen-nanopb={NANOPB_PLUGIN}',
-                f'--nanopb_out={PBGEN_MC}',
-                str(pb_file)
-            ], check=True)
+        run([
+            'protoc',
+            f'-I={PB_DIR}',
+            f'--python_out={PBGEN_PY}',
+            str(pb_file)
+        ], check=True)
+
+        run([
+            'protoc',
+            f'-I={PB_DIR}',
+            f'--plugin=protoc-gen-nanopb={NANOPB_PLUGIN}',
+            f'--nanopb_out={PBGEN_MC}',
+            str(pb_file)
+        ], check=True)
 
 if __name__ == "__main__":
-    build_protobuf(mc=False)
+    build_protobuf()
